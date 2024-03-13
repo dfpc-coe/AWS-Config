57b0e3d8600c22c4a90a718cb5786579cd30c7fb
ingalls <nick@ingalls.ca>
2024-03-12 18:11:20 -0600

    Add index file

--- cloudformation/lib/config.js
+++ cloudformation/lib/config.js
@@ -1,17 +1,94 @@
 import cf from '@openaddresses/cloudfriend';
+import fs from 'node:fs';
 
 const resources = {
-    Parameters: {
-        RoleName: {
-            Description: 'Name of the Role to associate with ConfigRecorded',
-            Default: 'aws-controltower-ConfigRecorderRole',
-            Type: 'String'
-        }
-    },
     Resources: {
+        EventNotify: {
+            Type: 'AWS::SNS::Topic',
+            Properties: {
+                TopicName: cf.stackName,
+                DisplayName: cf.stackName
+            }
+        },
+        EventRule: {
+            Type: 'AWS::Events::Rule',
+            Properties: {
+                Name: cf.stackName,
+                State: 'ENABLED',
+                Description: 'Check AWS Config Rules and check for violations',
+                ScheduleExpression: 'rate(5 minutes)',
+                Targets: [{
+                    Id: 'ConfigChecker',
+                    Arn: cf.getAtt('EventFunction', 'Arn')
+                }]
+            }
+        },
+        EventFunctionRole: {
+            Type: 'AWS::IAM::Role',
+            Properties: {
+                AssumeRolePolicyDocument: {
+                    Version: '2012-10-17',
+                    Statement: [{
+                        Effect: 'Allow',
+                        Principal: {
+                            Service: 'lambda.amazonaws.com'
+                        },
+                        Action: 'sts:AssumeRole'
+                    }]
+                },
+                Policies: [{
+                    PolicyName: cf.stackName,
+                    PolicyDocument: {
+                        Statement: [{
+                            Effect: 'Allow',
+                            Resource: '*',
+                            Action: [
+                                'config:DescribeConfigRules',
+                                'config:GetComplianceDetailsByConfigRule',
+                                'config:DescribeComplianceByConfigRule'
+                            ]
+                        },{
+                            Effect: 'Allow',
+                            Resource: [ cf.ref('EventNotify')],
+                            Action: ['sns:Publish']
+                        }]
+                    }
+                }]
+            }
+        },
+        EventFunctionLogs: {
+            Type: 'AWS::Logs::LogGroup',
+            Properties: {
+                LogGroupName: cf.join(['/aws/lambda/', cf.stackName]),
+                RetentionInDays: 7
+            }
+        },
+        EventFunction: {
+            Type: 'AWS::Lambda::Function',
+            DependsOn: ['EventFunctionLogs'],
+            Properties: {
+                FunctionName: cf.stackName,
+                Description: 'Check AWS Config Rules and check for violations',
+                MemorySize: 128,
+                Runtime: 'nodejs20.x',
+                Role: cf.getAtt('EventFunctionRole', 'Arn'),
+                Handler: 'index.handler',
+                Timeout: 30,
+                Environment: {
+                    Variables: {
+                        AWS_REGION: cf.region,
+                        AWS_ACCOUNT_ID: cf.accountId,
+                        TopicArn: cf.ref('EventNotify')
+                    }
+                },
+                Code: {
+                    ZipFile: String(fs.readFileSync(new URL('../../index.js', import.meta.url)))
+                }
+            }
+        },
         RequiredTags: {
-            "Type": "AWS::Config::ConfigRule",
-            "Properties": {
+            Type: "AWS::Config::ConfigRule",
+            Properties: {
                 ConfigRuleName: "Required-Tags",
                 Description: "This rule ensures resources have required tags",
                 InputParameters: {
---
+++ index.js
@@ -0,0 +1,39 @@
+const Config = require('@aws-sdk/client-config-service');
+const SNS = require('@aws-sdk/client-sns');
+const { randomUUID } = require('node:crypto');
+
+async function handler() {
+    const config = new Config.ConfigServiceClient({ region: process.env.AWS_REGION || process.env.AWS_DEFAULT_REGION });
+    const sns = new SNS.SNSClient({ region: process.env.AWS_REGION || process.env.AWS_DEFAULT_REGION });
+
+    const rules = await config.send(new Config.DescribeConfigRulesCommand({}));
+
+    for (const rule of rules.ConfigRules) {
+        let res = {}
+        do {
+            res = await config.send(new Config.GetComplianceDetailsByConfigRuleCommand({
+                NextToken: res.NextToken,
+                ConfigRuleName: rule.ConfigRuleName,
+                ComplianceTypes: ['NON_COMPLIANT']
+            }));
+
+            await sns.send(new SNS.PublishBatchCommand({
+                TopicArn: process.env.TopicArn,
+                PublishBatchRequestEntries: res.EvaluationResults.map((e) => {
+                    const f = e.EvaluationResultIdentifier.EvaluationResultQualifier;
+                    return {
+                        Id: f.ResourceId.replace(':', '-'),
+                        Subject: `ALARM: \"${f.ConfigRuleName}:${f.ResourceId}\"`,
+                        Message: `A Resource (${f.ResourceType}) with ARN ${f.ResourceId} is violating the ${f.ConfigRuleName} rule`
+                    };
+                })
+            }));
+        } while (res.NextToken)
+
+        return true;
+    }
+}
+
+module.exports = {
+    handler
+};
